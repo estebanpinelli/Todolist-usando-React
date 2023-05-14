4246583e6bde67e5758c8d1331bdf001eb52fb0d
Esteban Pinelli <81484588+estebanpinelli@users.noreply.github.com>
2023-05-14 20:12:56 +0000

    iniciado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
+
 const Home = () => {
+	
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	<div className="col-12 d-flex justify-content-center">
+	  <div className="row text-center">
+		<h1>todos</h1>
+				<input
+		  type="text"
+		  onChange={(e) => setInputValue(e.target.value)}
+		  value={inputValue}
+		  onKeyPress={(e) => {
+			if (e.key === "Enter") {
+			  setTodos(todos.concat([inputValue]));
+			  setInputValue("");
+			}
+		  }}
+		  placeholder="What needs to be done?"
+		/>
+		{todos.map((item, index) => (
+  <div key={index} className="card mb-3">
+    <div className="card-body p-1 m-1 d-flex justify-content-between">
+      {item}
+ 
+    <button type="button" className="btn-close " aria-label="Close" onClick={() =>
+      setTodos(todos.slice(0, -1))
+    }></button>
+	   </div>
+  </div>
+		))}
+	  </div>
+	  </div>
 	);
-};
+  };
+  
 
 export default Home;
